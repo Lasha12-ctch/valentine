955e3c3d78c4968dd7cea0aa2a22b0db16d4d0df
Lasha12-ctch <160390324+Lasha12-ctch@users.noreply.github.com>
2025-02-02 15:15:06 +0400

    Add files via upload

---
+++ Will-you-be-my-Valentine--main/script.js
@@ -0,0 +1,27 @@
+const messages = [
+    "Are you sure?",
+    "Think twice before you say no..",
+    "Is that really your final answer?",
+    "Babeee please...",
+    "Just think about it!",
+    "just imagine how happy you'd make me!",
+    "This is your last chance to say yes!",
+    "I will be very very very sad...",
+    "Ok fine, I will stop asking...",
+    "Just kidding, say yes please! ❤️"
+];
+
+let messageIndex = 0;
+
+function handleNoClick() {
+    const noButton = document.querySelector('.no-button');
+    const yesButton = document.querySelector('.yes-button');
+    noButton.textContent = messages[messageIndex];
+    messageIndex = (messageIndex + 1) % messages.length;
+    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+}
+
+function handleYesClick() {
+    window.location.href = "yes_page.html";
+}
